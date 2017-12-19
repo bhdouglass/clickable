215bc494ed839bd02e05ec8e7db282d9c2e77000
Brian Douglass <bhdouglass@gmail.com>
2017-12-19 16:47:52 -0500

    Updated packaging and removed usdk-target

--- publish_deb.sh
+++ publish_deb.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-sed -i 's/unstable/trusty/g' debian/changelog
-rm -f ../clickable_*
-debuild -S
-dput ppa:bhdouglass/clickable ../clickable_*_source.changes
-
-sed -i 's/trusty/xenial/g' debian/changelog
-rm -f ../clickable_*
-debuild -S
-dput ppa:bhdouglass/clickable ../clickable_*_source.changes
-
-sed -i 's/xenial/zesty/g' debian/changelog
-rm -f ../clickable_*
-debuild -S
-dput ppa:bhdouglass/clickable ../clickable_*_source.changes
-
-sed -i 's/zesty/artful/g' debian/changelog
+sed -i 's/unstable/artful/g' debian/changelog
 rm -f ../clickable_*
 debuild -S
 dput ppa:bhdouglass/clickable ../clickable_*_source.changes
