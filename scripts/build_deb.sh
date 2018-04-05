122706986c50b40c2a2c4d56846429427654533d
Brian Douglass <bhdouglass@gmail.com>
2018-04-05 11:43:59 -0400

    Updated publish script

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -4,8 +4,6 @@ docker run \
     -v `pwd`/../:`pwd`/../ \
     -w `pwd` \
     -u `id -u` \
-    -e PYBUILD_INSTALL_ARGS_python3=--install-scripts=/usr/bin/ \
-    -e PYBUILD_NAME=clickable \
     --rm \
     -it clickable/build-deb:python3 \
     dpkg-buildpackage
--- scripts/publish_deb.sh
+++ scripts/publish_deb.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+function docker_run {
+    docker run \
+        -v `pwd`/../:`pwd`/../ \
+        -v $HOME/.gnupg/:$HOME/.gnupg/ \
+        -w `pwd` \
+        -u `id -u` \
+        -e HOME=$HOME \
+        --rm \
+        -it clickable/build-deb:python3 $1
+}
+
 # Prepare for upload and build source package
 sed -i 's/unstable/artful/g' debian/changelog
-
-debuild -S
-dput ppa:bhdouglass/clickable ../clickable_*_source.changes
+docker_run "debuild -S"
+docker_run "dput ppa:bhdouglass/clickable ../clickable_*_source.changes"
 
 # Clean up
-dh_clean
+docker_run "dh_clean"
 sed -i 's/artful/unstable/g' debian/changelog
