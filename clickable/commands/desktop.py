3cb70d4488a8c580aa4176b44098a9641eceba0c
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2020-04-03 19:04:11 +0200

    Add custom command desktop mode

--- clickable/commands/desktop.py
+++ clickable/commands/desktop.py
@@ -30,12 +30,18 @@ class DesktopCommand(Command):
     name = 'desktop'
     help = 'Run the app on your desktop'
 
+    def __init__(self, config):
+        super().__init__(config)
+        self.command = None
+        self.custom_mode = False
+
     def run(self, path_arg=None):
         self.prepare_run()
         self.run_app()
 
     def prepare_run(self):
-        if not self.config.desktop_skip_build:
+        if not (self.config.desktop_skip_build
+                or self.custom_mode):
             if not self.config.dirty:
                 CleanCommand(self.config).run()
             BuildCommand(self.config).run()
@@ -54,18 +60,22 @@ class DesktopCommand(Command):
 
         docker_config.uid = os.getuid()
         docker_config.docker_image = self.config.container.docker_image
-        docker_config.working_directory = self.config.install_dir
         docker_config.use_nvidia = self.config.use_nvidia
 
-        docker_config.execute = self.determine_executable(
-            self.find_desktop_file()
-        )
+        if self.custom_mode:
+            docker_config.execute = self.command
+            docker_config.working_directory = self.config.root_dir
+        else:
+            docker_config.execute = self.determine_executable(
+                self.find_desktop_file()
+            )
+            docker_config.working_directory = self.config.install_dir
 
-        package_name = self.config.install_files.find_package_name()
+            WebappSupport(self.config.install_files.find_package_name()).update(docker_config)
+            ThemeSupport(self.config).update(docker_config)
+            DebugGdbSupport(self.config).update(docker_config)
 
-        docker_config.add_volume_mappings(
-            self.setup_volume_mappings(package_name)
-        )
+        docker_config.add_volume_mappings(self.setup_volume_mappings())
 
         docker_config.add_environment_variables(
             self.setup_environment(docker_config.working_directory)
@@ -73,15 +83,9 @@ class DesktopCommand(Command):
 
         NvidiaSupport().update(docker_config)
 
-        WebappSupport(package_name).update(docker_config)
-
         GoSupport(self.config).update(docker_config)
 
         RustSupport(self.config).update(docker_config)
-
-        DebugGdbSupport(self.config).update(docker_config)
-
-        ThemeSupport(self.config).update(docker_config)
 
         MultimediaSupport(self.config).update(docker_config)
 
@@ -148,7 +152,7 @@ class DesktopCommand(Command):
     def setup_environment(self, working_directory):
         lib_path = self.get_docker_lib_path_env(working_directory)
 
-        return {
+        env_vars = {
             'LANG': self.config.desktop_locale,
             'LANGUAGE': self.config.desktop_locale,
             'LC_CTYPE': self.config.desktop_locale,
@@ -177,6 +181,12 @@ class DesktopCommand(Command):
             'UBUNTU_APP_LAUNCH_ARCH': 'x86_64-linux-gnu',
         }
 
+        if self.custom_mode:
+            env_vars.update(self.config.get_env_vars())
+            env_vars['HOME'] = os.environ['HOME']
+
+        return env_vars
+
     def get_docker_lib_path_env(self, working_directory):
         return ':'.join([
             os.path.join(working_directory, 'lib/x86_64-linux-gnu'),
@@ -195,19 +205,26 @@ class DesktopCommand(Command):
             '/usr/bin',
         ])
 
-    def setup_volume_mappings(self, package_name):
+    def setup_volume_mappings(self):
         xauth_path = self.touch_xauth()
 
         device_home = Constants.desktop_device_home
         makedirs(device_home)
         logger.info("Mounting device home to {}".format(device_home))
 
-        return {
+        vol_map = {
             self.config.cwd: self.config.cwd,
             '/tmp/.X11-unix': '/tmp/.X11-unix',
             xauth_path: xauth_path,
             device_home: '/home/phablet',
+            '/etc/passwd': '/etc/passwd',
         }
+
+        if self.custom_mode:
+            user_home = os.path.expanduser('~')
+            vol_map[user_home] = user_home
+
+        return vol_map
 
     def touch_xauth(self):
         xauth_path = '/tmp/.docker.xauth'
