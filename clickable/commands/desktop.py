626eef08d418a5d91c3d9a9a46f812364c16ac48
Lionel Duboeuf <lionel.duboeuf@actility.com>
2020-06-08 16:51:16 +0200

    Manage qtcreator initialization process and build/run template creation here

--- clickable/commands/desktop.py
+++ clickable/commands/desktop.py
@@ -35,6 +35,7 @@ class DesktopCommand(Command):
         super().__init__(config)
         self.command = None
         self.custom_mode = False
+        self.idedelegate = None
 
     def run(self, path_arg=None):
         self.prepare_run()
@@ -239,5 +240,8 @@ class DesktopCommand(Command):
         docker_config = self.setup_docker()
         command = docker_config.render_command()
         logger.debug(command)
+
+        if self.idedelegate is not None:
+            self.idedelegate.before_run(docker_config)
 
         subprocess.check_call(shlex.split(command), cwd=docker_config.working_directory)
