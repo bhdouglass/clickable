0810c9b9816fe3fdd7293c05cff2b236f5b9d25d
Lionel Duboeuf <lionel.duboeuf@actility.com>
2020-06-08 16:45:16 +0200

    Manage qtcreator initialization process and build/run template creation here

---
+++ tests/unit/test_ide_qtcreator.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+from clickable.commands.ide import IdeCommand
+from clickable.commands.docker.docker_config import DockerConfig
+from clickable.commands.idedelegates.qtcreator import QtCreatorDelegate
+from .base_test import UnitTest
+from unittest import mock
+
+
+class TestIdeQtCreatorCommand(UnitTest):
+
+    def setUp(self):
+        self.setUpConfig()
+        self.command = IdeCommand(self.config)
+        self.command.prepare_run()
+        self.command.prepare_run()
+        self.docker_config = self.command.setup_docker()
+
+        self.idedelegate = QtCreatorDelegate()
+        self.idedelegate.clickable_dir = '/tmp/tests/.clickable'
+        self.idedelegate.project_path = '/tmp/tests/qmlproject'
+        self.idedelegate.target_settings_path = os.path.join(self.idedelegate.clickable_dir ,'QtProject')
+
+        os.mkdir(self.idedelegate.project_path)
+
+
+    def test_command_overrided(self):
+
+        path_arg =  self.idedelegate.override_command('qtcreator')
+
+        self.assertEqual(path_arg, 'qtcreator -settingspath {} {}'.format(self.idedelegate.clickable_dir, self.idedelegate.project_path))
+
+    def test_initialize_qtcreator_conf(self):
+
+        self.idedelegate.before_run(self.docker_config)
+        self.assertTrue(os.path.isdir('/tmp/tests/.clickable/QtProject'))
+
+    def test_init_cmake_project(self):
+
+        output_file = os.path.join(self.idedelegate.project_path, 'CMakeLists.txt.user.shared')
+
+
+        #if Exec not found in desktop, should do nothing
+        self.docker_config.environment.pop('CLICK_EXEC', None)
+        self.idedelegate.init_cmake_project(self.docker_config)
+        self.assertFalse(os.path.isfile(output_file))
+
+        #normal case
+        self.docker_config.add_environment_variables(
+            {
+                "CLICK_EXEC": "qmlscene",
+                "CLICK_EXEC_PARAMS": "qml/Main.qml",
+            })
+
+        self.idedelegate.init_cmake_project(self.docker_config)
+        self.assertTrue(os.path.isfile(output_file))
+        #test an example variable that have been replaced
+        self.assertTrue(open(output_file, 'r').read().find('CustomExecutableRunConfiguration.Arguments">qmlscene</value>'))
+
+    def tearDown(self):
+        shutil.rmtree(self.idedelegate.project_path, ignore_errors=True)
