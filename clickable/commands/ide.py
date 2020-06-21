2ee8c8df2c1df4a56fde8d9b4c5bd8ba3e3ef3fd
Lionel Duboeuf <lionel.duboeuf@actility.com>
2020-06-21 23:01:49 +0200

    update log message

--- clickable/commands/ide.py
+++ clickable/commands/ide.py
@@ -3,7 +3,6 @@ from clickable.logger import logger
 from clickable.exceptions import ClickableException
 from .idedelegates.qtcreator import QtCreatorDelegate
 
-
 class IdeCommand(DesktopCommand):
     aliases = []
     name = 'ide'
@@ -21,7 +20,7 @@ class IdeCommand(DesktopCommand):
         if 'qtcreator' in path_arg.split():
             self.ide_delegate = QtCreatorDelegate()
             path_arg = self.ide_delegate.override_command(path_arg)
-            logger.debug('new ide command: {}'.format(path_arg))
+            logger.debug('QtCreator command detected. Changing command to: {}'.format(path_arg))
 
         self.command = path_arg
         super().run()
