e6075497e97098ce68d33143b8d99de11a0be2fc
Brian Douglass <bhdouglass@gmail.com>
2020-08-01 23:28:50 -0400

    Bumped version to 6.17.0

--- clickable/__init__.py
+++ clickable/__init__.py
@@ -15,7 +15,7 @@ from clickable.logger import logger, log_file, console_handler
 from clickable.exceptions import ClickableException
 
 
-__version__ = '6.16.0'
+__version__ = '6.17.0'
 __container_minimum_required__ = 2
 
 
