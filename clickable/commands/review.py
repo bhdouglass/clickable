24fd1f02f4a779c8d808f8a2f46819931f748062
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2020-09-18 22:48:10 +0200

    Run review after build (opt-out)

--- clickable/commands/review.py
+++ clickable/commands/review.py
@@ -9,10 +9,10 @@ class ReviewCommand(Command):
     name = 'review'
     help = 'Takes the built click package and runs click-review against it'
 
-    def run(self, path_arg=None):
-        if path_arg:
-            click = os.path.basename(path_arg)
-            click_path = path_arg
+    def check(self, path=None, raise_on_error=False, raise_on_warning=False):
+        if path:
+            click = os.path.basename(path)
+            click_path = path
         else:
             click = self.config.install_files.get_click_filename()
             click_path = os.path.join(self.config.build_dir, click)
@@ -22,7 +22,12 @@ class ReviewCommand(Command):
         try:
             self.config.container.run_command('click-review {}'.format(click_path), use_build_dir=False, cwd=cwd)
         except subprocess.CalledProcessError as e:
-            if e.returncode == 2 or e.returncode == 3:
+            if e.returncode == 2 and not raise_on_error:
+                pass
+            elif e.returncode == 3 and not raise_on_warning:
                 pass
             else:
                 raise e
+
+    def run(self, path_arg=None):
+        self.check(path_arg)
